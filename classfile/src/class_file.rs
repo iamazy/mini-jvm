5ca4d8b59ae78c6694fc7c2bf82b3aa156b4b40f
iamazy <1448588084@qq.com>
2020-12-14 19:23:23 +0800

    Update class_file.rs

--- classfile/src/class_file.rs
+++ classfile/src/class_file.rs
@@ -1,7 +1,9 @@
 use crate::constant::Constant;
 use crate::field::Field;
+use crate::method::Method;
+use crate::attribute::Attribute;
 
-#[derive(Debug)]
+#[derive(Debug,)]
 pub struct ClassFile {
     pub magic: u32,
     pub minor_version: u16,
@@ -11,7 +13,7 @@ pub struct ClassFile {
     pub this_class: u16,
     pub super_class: u16,
     pub interfaces: Vec<Constant>,
-    pub fields_count: u16,
-    pub fields: Vec<Field>
-
+    pub fields: Vec<Field>,
+    pub methods: Vec<Method>,
+    pub attributes: Vec<Attribute>
 }
