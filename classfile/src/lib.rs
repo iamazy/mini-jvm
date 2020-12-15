49d4426d516bc3c77ff74cffe16608ac905d6f01
iamazy <1448588084@qq.com>
2020-12-16 00:05:09 +0800

    Update lib.rs

--- classfile/src/lib.rs
+++ classfile/src/lib.rs
@@ -1,4 +1,4 @@
-use bytes::{BufMut, BytesMut};
+use bytes::{BufMut, BytesMut, Buf, Bytes};
 use crate::error::Error;
 
 pub mod class_file;
@@ -10,7 +10,33 @@ pub mod ops;
 pub mod class_reader;
 pub mod error;
 
-pub trait Frame<R> {
+
+pub fn write_string(string: String, buf: &mut impl BufMut) -> usize {
+    write_bytes(Bytes::from(string), buf)
+}
+
+pub fn read_string(buf: &mut BytesMut) -> Result<String, Error> {
+    String::from_utf8(read_bytes(buf)?.to_vec())
+        .map_err(|e| Error::InvalidString(e.utf8_error().to_string()))
+}
+
+pub fn write_bytes(bytes: Bytes, buf: &mut impl BufMut) -> usize {
+    let len = bytes.len();
+    buf.put_u16(len as u16);
+    buf.put_slice(bytes.bytes());
+    len + 2
+}
+
+pub fn read_bytes(buf: &mut BytesMut) -> Result<Bytes, Error> {
+    let len = buf.get_u16() as usize;
+    if len > buf.remaining() {
+        Err(Error::InvalidLength)
+    } else {
+        Ok(buf.split_to(len).split().freeze())
+    }
+}
+
+pub trait FromToBytes<R> {
     fn to_buf(&self, buf: &mut impl BufMut) -> usize;
     fn from_buf(buf: &mut BytesMut) -> Result<R, Error>;
     fn length(&self) -> usize;
