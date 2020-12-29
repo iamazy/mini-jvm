e467fb515d1d04453a4ddde71fe48eb2d935a32f
iamazy <1448588084@qq.com>
2020-12-29 08:17:54 +0800

    Update mod.rs

--- hotspot/src/runtime/mod.rs
+++ hotspot/src/runtime/mod.rs
@@ -4,15 +4,10 @@ use std::fmt::{self, Display, Formatter};
 pub mod thread;
 pub mod frame;
 pub mod local_vars;
+pub mod constant_pool;
 
 #[derive(Debug, Clone)]
 pub enum Slot {
     Oop(Oop),
     Nop
-}
-
-impl Display for Slot {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        unimplemented!()
-    }
 }
