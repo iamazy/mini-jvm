dd1518ccb7d3defee77d02df761d904b62494ebe
iamazy <1448588084@qq.com>
2020-12-30 19:22:46 +0800

    Update access_flags.rs

--- classfile/src/access_flags.rs
+++ classfile/src/access_flags.rs
@@ -4,109 +4,84 @@ pub struct AccessFlags(u16);
 impl AccessFlags {
 
     pub fn is_public(&self) -> bool {
-        self.0 & AccessFlag::Public.into() != 0
+        self.0 & AccessFlag::ACC_PUBLIC.bits != 0
     }
 
     pub fn is_private(&self) -> bool {
-        self.0 & AccessFlag::Private.into() != 0
+        self.0 & AccessFlag::ACC_PRIVATE.bits != 0
     }
 
     pub fn is_protected(&self) -> bool {
-        self.0 & AccessFlag::Protected.into() != 0
+        self.0 & AccessFlag::ACC_PROTECTED.bits != 0
     }
 
     pub fn is_static(&self) -> bool {
-        self.0 & AccessFlag::Static.into() != 0
+        self.0 & AccessFlag::ACC_STATIC.bits != 0
     }
 
     pub fn is_final(&self) -> bool {
-        self.0 & AccessFlag::Final.into() != 0
+        self.0 & AccessFlag::ACC_FINAL.bits != 0
     }
 
     pub fn is_synchronized(&self) -> bool {
-        self.0 & AccessFlag::Synchronized.into() != 0
+        self.0 & AccessFlag::ACC_SYNCHRONIZED.bits != 0
     }
 
     pub fn is_super(&self) -> bool {
-        self.0 & AccessFlag::Super.into() != 0
+        self.0 & AccessFlag::ACC_SUPER.bits != 0
     }
 
     pub fn is_volatile(&self) -> bool {
-        self.0 & AccessFlag::Volatile.into() != 0
+        self.0 & AccessFlag::ACC_VOLATILE.bits != 0
     }
 
     pub fn is_transient(&self) -> bool {
-        self.0 & AccessFlag::Transient.into() != 0
+        self.0 & AccessFlag::ACC_TRANSIENT.bits != 0
     }
 
     pub fn is_native(&self) -> bool {
-        self.0 as usize & AccessFlag::Native.into() != 0
+        self.0 & AccessFlag::ACC_NATIVE.bits != 0
     }
 
     pub fn is_interface(&self) -> bool {
-        self.0 & AccessFlag::Interface.into() != 0
+        self.0 & AccessFlag::ACC_INTERFACE.bits != 0
     }
 
     pub fn is_abstract(&self) -> bool {
-        self.0 & AccessFlag::Abstract.into() != 0
+        self.0 & AccessFlag::ACC_ABSTRACT.bits != 0
     }
 
     pub fn is_strict(&self) -> bool {
-        self.0 & AccessFlag::Strict.into() != 0
+        self.0 & AccessFlag::ACC_STRICT.bits != 0
     }
 
     // Attribute flags
     pub fn is_synthetic(&self) -> bool {
-        self.0 & AccessFlag::Synthetic.into() != 0
+        self.0 & AccessFlag::ACC_SYNTHETIC.bits != 0
     }
 
 }
 
-/// Class access and property modifiers
-#[derive(Debug, Clone)]
-pub enum AccessFlag {
-    Public,
-    Private,
-    Protected,
-    Static,
-    Final,
-    Super,
-    Synchronized,
-    Bridge,
-    Volatile,
-    Varargs,
-    Transient,
-    Native,
-    Interface,
-    Abstract,
-    Strict,
-    Synthetic,
-    Annotation,
-    Enum
-}
-
-impl Into<u16> for AccessFlag {
-    fn into(self) -> u16 {
-        match self {
-            AccessFlag::Public => 0x0001u16,
-            AccessFlag::Private => 0x0002u16,
-            AccessFlag::Protected => 0x0004u16,
-            AccessFlag::Static => 0x0008u16,
-            AccessFlag::Final => 0x0010u16,
-            AccessFlag::Super => 0x0020u16,
-            AccessFlag::Synchronized => 0x0020u16,
-            AccessFlag::Bridge => 0x0040u16,
-            AccessFlag::Volatile => 0x0040u16,
-            AccessFlag::Varargs => 0x0080u16,
-            AccessFlag::Transient => 0x0080u16,
-            AccessFlag::Native => 0x0100u16,
-            AccessFlag::Interface => 0x0200u16,
-            AccessFlag::Abstract => 0x0400u16,
-            AccessFlag::Strict => 0x0800u16,
-            AccessFlag::Synthetic => 0x1000u16,
-            AccessFlag::Annotation => 0x2000u16,
-            AccessFlag::Enum => 0x4000u16
-        }
+bitflags! {
+    struct AccessFlag: u16 {
+        const ACC_PUBLIC = 0x0001;
+        const ACC_PRIVATE = 0x0002;
+        const ACC_PROTECTED = 0x0004;
+        const ACC_STATIC = 0x0008;
+        const ACC_FINAL = 0x0010;
+        const ACC_SUPER = 0x0020;
+        const ACC_SYNCHRONIZED = 0x0020;
+        const ACC_BRIDGE = 0x0040;
+        const ACC_VOLATILE = 0x0040;
+        const ACC_VARARGS = 0x0080;
+        const ACC_TRANSIENT = 0x0080;
+        const ACC_NATIVE = 0x0100;
+        const ACC_INTERFACE = 0x0200;
+        const ACC_ABSTRACT = 0x0400;
+        const ACC_STRICT = 0x0800;
+        const ACC_SYNTHETIC = 0x1000;
+        const ACC_ANNOTATION = 0x2000;
+        const ACC_ENUM = 0x4000;
     }
 }
 
