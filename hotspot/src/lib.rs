80cc0562d3cee5e30f2e7e3a3b94bcef503ebaf3
iamazy <1448588084@qq.com>
2021-01-03 01:13:01 +0800

    Update lib.rs

--- hotspot/src/lib.rs
+++ hotspot/src/lib.rs
@@ -1,10 +1,14 @@
-#[macro_use]
-pub mod macros;
+extern crate log;
+extern crate simplelog;
+
 pub mod basic_type;
 pub mod classpath;
+#[macro_use]
+pub mod macros;
 pub mod oops;
 pub mod runtime;
 pub mod stack;
+pub mod sys;
 pub mod types;
 
 #[cfg(test)]
